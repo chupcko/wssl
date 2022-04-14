340a7606e27437379b98efb4cee23fa2d3b9816d
Goran Lazic <github@chupcko.org>
2022-04-14 15:24:13 +0200

    Fixed HTML

--- html/index.js
+++ html/index.js
@@ -1,6 +1,4 @@
 var version = '20170514';
 
-/* var serverIPV4Name = 'wssl4.chupcko.org'; */
-/* var serverIPV6Name = 'wssl6.chupcko.org'; */
 var serverIPV4Name = '127.0.0.1';
 var serverIPV6Name = '[::1]';
